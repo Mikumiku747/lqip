a6882b8620a202e76303a7b2dd892d444091f999
Zouhir <zouhir@zouhir.org>
2017-09-13 21:06:08 +1000

    minor update on yaml fs

--- file-stream.h
+++ file-stream.h
@@ -1,5 +1,5 @@
 /**
- * @name writeToFIle
+ * @name writeToFile
  * @brief accepts an array of images struct and writes it to file in YAML format.
  *
  * @developer:
@@ -8,4 +8,17 @@
  * @param width
  * @param height
  */
-void writeToFIle(images_t *images);
+void writeToFile(images_t *images);
+
+
+/**
+ * @name constructYaml
+ * @brief accepts key and a value and returns a valid Yaml char array
+ *
+ * @developer:
+ *
+ * @param uri
+ * @param width
+ * @param height
+ */
+char *constructYaml(char *key, char* val);
